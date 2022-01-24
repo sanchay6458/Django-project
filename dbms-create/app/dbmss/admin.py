05cee0288cbef266f603756e2f2a8e46fcba9a3d
Sanchay Upadhyay <sanchaydev67@gmail.com>
2022-01-23 19:27:38 -0800

    add_temp_dbms

--- dbms-create/app/dbmss/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import dbms1
-
-admin.site.register(dbms1)
-
-# Register your models here.
--- dbms-create/app/index/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', indexpage.as_view() ,name='indexs'),
-    path('about/', about.as_view() , name='about'),
-    path('contact/', contact.as_view() , name='contact'),
-    path('more/', more.as_view() , name='more')
-]
